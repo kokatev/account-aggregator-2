355d0a8074321738d3ea7ad2f92ea446d6246794
kokatev <vakow.vish123@gmail.com>
2020-09-17 00:54:52 +0530

    Add files via upload

---
+++ graphSummary.component.ts
@@ -0,0 +1,139 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { SharedService } from "./shared.service";
+import * as pluginDataLabels from 'chartjs-plugin-datalabels';
+import { monthlySavingsDetails } from "./mock.data";
+
+@Component({
+  selector: 'graph-summary',
+  templateUrl: 'graphSummary.component.html',
+})
+
+export class GraphSummaryComponent implements OnInit {
+
+  public monthLongNames = {
+    'Jan': 'January',
+    'Feb': 'February',
+    'Mar': 'March',
+    'Apr': 'April',
+    'May': 'May',
+    'Jun': 'June'
+  };
+
+  public monthlySavingsChartType = 'bar';
+  public monthlySavingsChartLabels: Array<any> = [];
+  public monthlySavingsChartData: Array<any> = [
+    {
+      data: [],
+      label: 'Monthly Savings',
+      borderWidth: 2,
+      barThickness: 30
+    }
+  ];
+  public monthlySavingsChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales : {
+      yAxes: [
+        {
+          ticks: {
+            max : 100000,
+            min : 0,
+            fontColor: 'black'
+          },
+          gridLines: {
+              color: "rgba(169,169,169,0.2)",
+          }
+        }
+      ],
+      xAxes: [
+        {
+          ticks: {
+            fontColor: 'black'
+          },
+          gridLines: {
+              color: "rgba(0, 0, 0, 0)",
+          }
+        }
+      ]
+    },
+    legend: {
+        display: true,
+        labels: {
+            fontColor: 'black'
+        },
+        position: 'top',
+    }
+  };
+
+  public savingsInMonthChartType = 'doughnut';
+  public savingsInMonthChartLabels: Array<any> = [];
+  public savingsInMonthChartData = [
+    {
+      data: [],
+    }
+  ];
+  public monthToBeDisplayed = 'Jun';
+  public savingsInMonthChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+        display: true,
+        labels: {
+            fontColor: 'white',
+            padding: 20
+        },
+        position: 'bottom',
+    }
+  };
+
+  constructor(
+    public sharedService: SharedService
+  ) { }
+
+  ngOnInit() {
+    this.sharedService.screenName = 'Summary';
+    Object.keys(monthlySavingsDetails).forEach(
+      (eachMonth) => {
+        this.monthlySavingsChartLabels.push(eachMonth);
+        let totalMonthlyAmount = 0;
+        Object.keys(monthlySavingsDetails[eachMonth]).forEach(
+          (eachEntity) => {
+            totalMonthlyAmount += monthlySavingsDetails[eachMonth][eachEntity].amount
+          }
+        );
+        this.monthlySavingsChartData[0].data.push(totalMonthlyAmount);
+      }
+    );
+    Object.keys(monthlySavingsDetails[this.monthToBeDisplayed]).forEach(
+      (eachEntity)=>{
+        this.savingsInMonthChartLabels.push(eachEntity);
+        this.savingsInMonthChartData[0].data.push(monthlySavingsDetails[this.monthToBeDisplayed][eachEntity].amount)
+      }
+    );
+  }
+
+  onClickMonthlySavingsChart(chartData){
+    if(chartData.active.length > 0){
+      this.monthToBeDisplayed = chartData.active[0]._model.label;
+      this.updateSavingsInMonthChart();
+    }
+  }
+
+  updateSavingsInMonthChart(){
+    let updatedSavingsInMonthChartLabels = [];
+    let updatedSavingsInMonthChartData= [];
+    Object.keys(monthlySavingsDetails[this.monthToBeDisplayed]).forEach(
+      (eachEntity)=>{
+        updatedSavingsInMonthChartLabels.push(eachEntity);
+        updatedSavingsInMonthChartData.push(monthlySavingsDetails[this.monthToBeDisplayed][eachEntity].amount)
+      }
+    );
+    this.savingsInMonthChartLabels = updatedSavingsInMonthChartLabels;
+    this.savingsInMonthChartData = [
+      {
+        data : updatedSavingsInMonthChartData
+      }
+    ];
+  }
+
+}
---
+++ mock.data2.ts
@@ -0,0 +1,91 @@
+export const transactions = [
+{
+  txnDate: '20-Sep-2020',
+  txnAmount: 'Rs. 2098.00',
+  txnType: 'Debit',
+  txnID: 'KPR345',
+  bank: 'icici',
+  description: 'Paid to AirportFoodClub'
+}
+];
+
+export const bankLogos = {
+icici:{
+  logo: 'https://www.logotaglines.com/wp-content/uploads/2016/08/ICICI-Logo-1200x1227.png'
+},
+hdfc: {
+  logo: 'https://www.logotaglines.com/wp-content/uploads/2016/08/HDFC_Bank_Logo-e1470986841531-1200x1528.jpg'
+},
+paytm: {
+  logo: 'https://pbs.twimg.com/profile_images/1241781545607692289/UUQuPbwY_400x400.jpg'
+}
+};
+
+export const monthlySavingsDetails = {
+  'Jan': {
+    'ICICI': {
+      amount: 20000
+    },
+    'HDFC': {
+      amount: 15000
+    },
+    'PAYTM': {
+      amount: 28000
+    }
+  },
+  'Feb': {
+    'ICICI': {
+      amount: 25000
+    },
+    'HDFC': {
+      amount: 50000
+    },
+    'PAYTM': {
+      amount: 2000
+    }
+  },
+  'Mar': {
+    'ICICI': {
+      amount: 20000
+    },
+    'HDFC': {
+      amount: 35000
+    },
+    'PAYTM': {
+      amount: 4000
+    }
+  },
+  'Apr': {
+    'ICICI': {
+      amount: 26000
+    },
+    'HDFC': {
+      amount: 11000
+    },
+    'PAYTM': {
+      amount: 2000
+    }
+  },
+  'May': {
+    'ICICI': {
+      amount: 31500
+    },
+    'HDFC': {
+      amount: 48500
+    },
+    'PAYTM': {
+      amount: 10000
+    }
+  },
+  'Jun': {
+    'ICICI': {
+      amount: 2500
+    },
+    'HDFC': {
+      amount: 15000
+    },
+    'PAYTM': {
+      amount: 2500
+    }
+  }
+};
